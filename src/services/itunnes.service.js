fd5271692165b3f95115e72708f31771bcae952a
yurians19 <yurians19@gmail.com>
2020-07-16 15:21:34 -0400

    add unique show in array tvmaze

--- src/services/itunnes.service.js
+++ src/services/itunnes.service.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 module.exports = async (term,entity) => {
   const URL = `https://itunes.apple.com/search`
   try {
-    const response = await axios.get(URL,{params:{term,entity}})
-    return response.data
+    const {data} = await axios.get(URL,{params:{term,entity}})
+    return data
   } catch (error) {
     console.log('error',error);
     throw Error(error)
--- src/services/tvmaze.service.js
+++ src/services/tvmaze.service.js
@@ -4,8 +4,8 @@ const { setOriginEntity } = require('../lib')
 module.exports = async q => {
   const URL = `http://api.tvmaze.com/search/shows`
   try {
-    const response = await axios.get(URL,{params:{q}})
-    return setOriginEntity(response.data,'shows','tvmaze')
+    const {data} = await axios.get(URL,{params:{q}})
+    return setOriginEntity([...new Set(data.map(item => item.show))],'shows','tvmaze')
   } catch (error) {
     throw error;
   }
