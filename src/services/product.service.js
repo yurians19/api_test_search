51a142a207faa9ca9124c28fd5f53517e8435c22
ygarcia2 <ygarcia@shokworks.io>
2020-11-04 13:31:12 -0400

    fix

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -4,11 +4,19 @@ const jsonfile = require('jsonfile')
 
 module.exports = async ({ code, qty ,url}) => {
   try {
+    let response = { status: "Out Stock"} 
     const {powerdistributors} = await jsonfile.readFile('login.json')
-    const {data} = await axios.request({url:`${url}/customapi/Product/Get/${code}`,params:{qty,cartType:'regular'},headers:{Cookie: powerdistributors}})
-    return data
+    const {data : res} = await axios.request({url:`${url}/customapi/Product/Autocomplete`,params:{search:code},headers:{Cookie: powerdistributors}})
+    const { Product: { Id } } = res[0]
+    const {data} = await axios.request({url:`${url}/customapi/Product/Get/${Id}`,params:{qty,cartType:'regular'},headers:{Cookie: powerdistributors}})
+    const {Product : {Availability}} = data
+    if (Availability >= qty) {
+      response.status = "In Stock"
+      response.quantity = Availability
+    }
+    return response
   } catch (error) {
-    console.log('error');
+    console.log('error',error);
     return null
   }
 }
