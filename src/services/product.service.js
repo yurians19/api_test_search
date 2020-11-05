46e325b6490c3fd074896b7939a711782620f394
ygarcia2 <ygarcia@shokworks.io>
2020-11-05 09:58:28 -0400

    fix

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -6,18 +6,18 @@ module.exports = async ({ code, qty ,url}) => {
   try {
     let response = { status: "Out Stock"} 
     const {powerdistributors} = await jsonfile.readFile('login.json')
-    // const res_ =await axios.delete({url:`${url}/customapi/Cart/EmptyCart`,params:{clearAdvite:false,cartType:'regular'},headers:{Cookie: powerdistributors}})
-    // console.log('res_',res_);
+    await axios.delete(`${url}/customapi/Cart/EmptyCart?cartType=regular?clearAdvite=false`,{headers:{Cookie: powerdistributors}})
     const {data : res} = await axios.request({url:`${url}/customapi/Product/Autocomplete`,params:{search:code},headers:{Cookie: powerdistributors}})
     const { Product: { Id } } = res[0]
     const {data} = await axios.request({url:`${url}/customapi/Product/Get/${Id}`,params:{qty,cartType:'regular'},headers:{Cookie: powerdistributors}})
-    console.log(data);
-    const {Product : {Availability}} = data
+    const {Product : {Availability,Supersedes,IsNLA}} = data
     if (Availability >= qty) {
       response.status = "In Stock"
+      response.supersedes = Supersedes
+      response.IsNLA = IsNLA
       response.quantity = Availability
     }
-    return data
+    return [data,res]
   } catch (error) {
     console.log('error',error);
     return null
