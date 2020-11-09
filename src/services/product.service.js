46404f874973165b8bdd2a9116beb83253ee7761
ygarcia2 <ygarcia@shokworks.io>
2020-11-09 14:44:50 -0400

    scrapping done

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const jsonfile = require('jsonfile')
+const cheerio = require('cheerio')
+const puppeteer = require('puppeteer')
+const Apify = require('apify');
+
 
 
 module.exports = async ({ code, qty ,url}) => {
@@ -7,19 +11,35 @@ module.exports = async ({ code, qty ,url}) => {
     let response = { status: "Out Stock"} 
     const {powerdistributors} = await jsonfile.readFile('login.json')
     await axios.delete(`${url}/customapi/Cart/EmptyCart?cartType=regular?clearAdvite=false`,{headers:{Cookie: powerdistributors}})
-    const {data : res} = await axios.request({url:`${url}/customapi/Product/Autocomplete`,params:{search:code},headers:{Cookie: powerdistributors}})
+    const {data : res} = await axios.request({url:`https://www.powerdistributors.com/customapi/Product/Autocomplete`,params:{search:code},headers:{Cookie: powerdistributors}})
     const { Product: { Id } } = res[0]
     const {data} = await axios.request({url:`${url}/customapi/Product/Get/${Id}`,params:{qty,cartType:'regular'},headers:{Cookie: powerdistributors}})
-    const {Product : {Availability,Supersedes,IsNLA}} = data
-    if (Availability >= qty) {
+    const {Product : {Supersedes,IsNLA}} = data
+    console.log('element',data,code,Id);
+    const browser = await puppeteer.launch(/* {headless: false} */)
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    await page.setCookie(  { name: '.AspNet.Cookies',
+    value:powerdistributors.slice(powerdistributors.indexOf('=')+1, powerdistributors.indexOf(';')),
+    domain: 'www.powerdistributors.com',
+    path: '/',
+    httpOnly: true,
+    secure: true,
+    session: false });
+    await page.goto('https://www.powerdistributors.com/portal/build-order'); // Opens page as logged user
+    let html = await page.content();
+    await browser.close();
+    let $ = await cheerio.load(html)
+    const element = $("#order-builder-search > div.ob__results > ul > li:nth-child(1) > div > div.ob__results__li.ob__results__center-result > i").attr('class');
+    if (element != 'ob__overview__icon alert fa fa-exclamation-triangle') {
       response.status = "In Stock"
       response.supersedes = Supersedes
       response.IsNLA = IsNLA
-      response.quantity = Availability
     }
-    return [data,res]
+    return response
   } catch (error) {
     console.log('error',error);
     return null
   }
 }
+
