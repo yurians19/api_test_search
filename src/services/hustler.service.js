8d0d38a23b0852a21dd6b6338c8e76ace906e99e
ygarcia2 <ygarcia@shokworks.io>
2020-11-18 15:32:03 -0400

    changes port 5550

--- src/services/hustler.service.js
+++ src/services/hustler.service.js
@@ -5,22 +5,21 @@ module.exports = async ({ code, qty }) => {
   try {
     let response = { status: "Out Stock",availability : 0}
     const {cookspower} = await jsonfile.readFile('loginHustler.json')
-    console.log(cookspower);
-    const {data/* : { lines } */} = await axios.request({  url:`https://www.cookspower.com/scs/services/LiveOrder.Service.ss`,
+    const {data: { lines }} = await axios.request({  url:`https://www.cookspower.com/scs/services/LiveOrder.Service.ss`,
                                           params:{c:code,n:qty},
                                           headers:{Cookie: cookspower}
                                         })
-    // if (lines.length == 0) {
-    //   return null
-    // }
-    // const {item: { isinstock }} = lines[0]
-    // const {quantity, rate} = lines[0]
-    // if (isinstock) {
-    //   response.ActualCost = rate
-    //   response.status = "In Stock"
-    //   response.availability = quantity
-    // }
-    return data
+    if (lines.length == 0) {
+      return null
+    }
+    const {item: { isinstock }} = lines[0]
+    const {quantity, rate} = lines[0]
+    if (isinstock) {
+      response.ActualCost = rate
+      response.status = "In Stock"
+      response.availability = quantity
+    }
+    return response
   } catch (error) {
     throw Error(error)
   }
