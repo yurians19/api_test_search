9b2ccc8a68d4b5fafbc1784536786122a73afbff
ygarcia2 <ygarcia@shokworks.io>
2020-11-18 13:08:25 -0400

    changes port 5550

--- src/services/hustler.service.js
+++ src/services/hustler.service.js
@@ -5,6 +5,7 @@ module.exports = async ({ code, qty }) => {
   try {
     let response = { status: "Out Stock",availability : 0}
     const {cookspower} = await jsonfile.readFile('loginHustler.json')
+    console.log(cookspower);
     const {data/* : { lines } */} = await axios.request({  url:`https://www.cookspower.com/scs/services/LiveOrder.Service.ss`,
                                           params:{c:code,n:qty},
                                           headers:{Cookie: cookspower}
